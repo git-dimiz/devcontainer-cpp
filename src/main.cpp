12176684320e3684ba7da4f8b4d52ad1dc33f3d3
git-dimiz <git.dimi@mailbox.org>
2024-03-15 13:47:58 +0100

    add support to debug target via vscode

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,14 @@
 #include <stdio.h>
 
 int main(int argc, char const *argv[]) {
-    (void)argc;
-    (void)argv;
-    printf("hello world\n");
+    if (argc != 1) {
+        printf("Args: ");
+        for (size_t i = 1; i < argc; i++) {
+            printf("%s ", argv[i]);
+        }
+        printf("\n");
+    } else {
+        printf("Hello World\n");
+    }
     return 0;
 }
