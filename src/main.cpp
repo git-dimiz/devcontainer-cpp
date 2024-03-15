67b08b417c0ed5d3701b4f0906a5d7b6a8c14b4a
git-dimiz <git.dimi@mailbox.org>
2024-03-15 12:28:05 +0100

    format code

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
+#include <functional>
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     (void)argc;
     (void)argv;
     printf("hello world\n");
